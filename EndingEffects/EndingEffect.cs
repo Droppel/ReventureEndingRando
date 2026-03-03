5fa6ddabba0c46bd43b08f77d94f4cb4b5293b95
droppel <leagueoflegends@sofortstart.de>
2026-03-03 16:04:04 +0100

    Reduce Chicken requirement to 1

--- EndingEffects/EndingEffect.cs
+++ EndingEffects/EndingEffect.cs
@@ -373,10 +373,7 @@ namespace ReventureEndingRando.EndingEffects
             GameObject.Destroy(phase2.GetComponent<AlterWithRestrictions>());
             GameObject.Destroy(phase3.GetComponent<AlterWithRestrictions>());
 
-            phase0.SetActive(effectsReceived == 1);
-            phase1.SetActive(effectsReceived == 2);
-            phase2.SetActive(effectsReceived == 3);
-            phase3.SetActive(effectsReceived >= 4);
+            phase3.SetActive(effectsReceived >= 1);
         }
     }
 
